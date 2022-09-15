815ad5e000f61f3f8024ecd93549fee76fa4b1c5
Lucilver <feliciaastrid2002@gmail.com>
2022-09-15 08:09:36 +0700

    Pengumpulan GD4_view_binding_10568

---
+++ app/src/main/java/com/example/view_binding_10568/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10568
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Guling-guling"),
+        Task("Chapter 2", "Buka lepi", "Nguli lagi"),
+        Task("Chapter 3", "Sarapan", "Sambil nguli"),
+        Task("Chapter 4", "Banyak freemos", "Gacha gacha gacha"),
+        Task("Chapter 5", "Kalah rate off", " Depreso Expreso")
+    )
+}
